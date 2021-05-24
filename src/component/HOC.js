e02f209acaf87a7c7c54ff1c0aac6cef005b7f7c
farukhpatel <farukhpatel0804@gmail.com>
2021-05-24 11:35:30 +0530

    m

--- src/component/HOC.js
+++ src/component/HOC.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import A from './A'
+//HOC is higher order component in which you update the functionality of existing component 
 const HOC=(ComponentABC)=>{
     const sem={
         semester:"6th",
